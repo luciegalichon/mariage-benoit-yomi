56e67e030dea7f1e1aff67a1699b09c6a6153985
lucie galichon <lgalichon@gmail.com>
2016-04-19 11:21:44 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,364 +1,42 @@
-Lieu.create(
-  name:'Camping des sources',
-  phone:'03 80 20 66 55',
-  url:'http://www.aquadis-loisirs.com/camping-des-sources/',
-  genre:'camping',
-  city:'Santenay',
-  description:"Avenue des Sources ( 9 minutes en voiture)"
-  )
-
-Lieu.create(
-  name:'Camping Le Pâquier Fané',
-  phone:'03 85 87 21 42',
-  url:'http://www.campingchagny.com/',
-  genre:'camping',
-  city:'Chagny',
-  description:"20, rue du Pâquier Fané 5 bungallow (6 minutes en voiture)"
-  )
-
-
-Lieu.create(
-  name:'Hameau de Blagny - Maison Maizières',
-  phone:'06 09 30 15 87',
-  url:'http://www.hameau-de-blagny.com/',
-  genre:'gite',
-  city:'Puligny-Montrachet',
-  description:"10, Hameau de Blagny (5 minutes en voiture, 6 personnes)"
-  )
-
-
-
-Lieu.create(
-  name:'Les Gageres',
-  phone:'03 80 21 97 46',
-  url:'http://www.chambres-hotes.fr/chambres-hotes_les-gageres_puligny-montrachet_30022.htm',
-  genre:'gite',
-  city:'Puligny-Montrachet',
-  description:"17 RUE DROUHIN (5 minutes en voiture, 60€ la chambre double)"
-  )
-
-Lieu.create(
-  name:'Le moulin saule',
-  phone:'03 80 20 60 17',
-  url:'http://www.gites-de-france.com/location-vacances-Santenay-Gite-Le-Moulin-Saule-21G638.html',
-  genre:'gite',
-  city:'Santenay',
-  description:"4, rue du Moulin Saule (6 minutes en voiture, 6 personnes)"
-  )
-Lieu.create(
-  name:'La Garance',
-  phone:'06 20 75 20 33',
-  url:'http://www.gitelagarance.com/',
-  genre:'gite',
-  city:'Santenay',
-  description:"1, rue Petit (10 minutes en voiture, 10 personnes)"
-  )
-
-Lieu.create(
-  name:'La mere Poulot',
-  phone:'03 80 45 97 15',
-  url:'http://www.gites-de-france.com/location-vacances-Santenay-Gite-La-Mere-Poulot-21G613.html',
-  genre:'gite',
-  city:'Santenay',
-  description:"2, rue de Narosse (9 minutes en voiture, 7 personnes)"
-  )
-
-Lieu.create(
-  name:'Gîte Clos Genêts',
-  phone:'resa par internet',
-  url:'http://www.gites-de-france.com/location-vacances-Santenay-Gite-Clos-Genets-21G951.html',
-  genre:'gite',
-  city:'Santenay',
-  description:"(10 minutes en voiture, 8 personnes)"
-  )
-
-Lieu.create(
-  name:'Le Gite du Montrachet',
-  phone:'03 80 21 31 71',
-  url:'http://www.legitedumontrachet.com/',
-  genre:'gite',
-  city:'Chassagne Montrachet',
-  description:"6 ROUTE DE SANTENAY  (2 minutes à pied, 6 personnes)"
-  )
-
-Lieu.create(
-  name:'Hôtel-Restaurant Le Montrachet',
-  phone:'03 80 21 30 06',
-  url:'http://www.le-montrachet.com/fr/',
-  genre:'hotel',
-  city:'Puligny-Montrachet',
-  description:"10, place des Marronniers, (5 minutes en voiture, plutôt haut de gamme)"
-  )
-
-Lieu.create(
-  name:'Hôtel La Chouette',
-  phone:'03 80 21 95 60',
-  url:'http://www.la-chouette.fr/fr/',
-  genre:'hotel',
-  city:'Puligny-Montrachet',
-  description:"3 bis, rue des Creux de Chagny (5 minutes en voiture, 145€ la chambre double)"
-  )
-
-
-
-Lieu.create(
-  name:'Maison d’Olivier Leflaive',
-  phone:'03 80 21 37 65',
-  url:'http://www.maison-olivierleflaive.fr/',
-  genre:'hotel',
-  city:'Puligny-Montrachet',
-  description: 'à 2 km du Château'
-  )
-
-Lieu.create(
-  name:'Domaine des Anges',
-  phone:'09 70 73 07 08',
-  genre:'hotel',
-  url:'http://www.domainedesangespuligny.com/',
-  city:'Puligny-Montrachet',
-  description: 'à 2 km du Château'
-  )
-
-Lieu.create(
-  name:'Domaine des Closeaux',
-  phone:'03 80 21 33 06',
-  url:'http://www.jacquesbavard.com/index.php?page=sejour&ssmenu=tarifs',
-  genre:'hotel',
-  city:'Puligny-Montrachet',
-  description: 'à 2 km du Château'
-  )
-
-
-Lieu.create(
-  name:'Les Gagères',
-  phone:'03 80 21 97 46',
-  url:'http://www.les-gageres.fr/nous-contacter.html',
-  genre:'hotel',
-  city:'Puligny-Montrachet',
-  description: 'à 2 km du Château'
-  )
-
-
-Lieu.create(
-  name:'Hôtel le Chagny',
-  phone:'03 85 87 67 67',
-  url:'http://hotel-le-chagny.fr/',
-  genre:'hotel',
-  city:'Chagny',
-  description: 'à 2 km du Château'
-  )
-
-Lieu.create(
-  name:'Auberge de la Musardière',
-  phone:'03 85 87 04 97',
-  url:'http://auberge.musardiere.pagesperso-orange.fr/',
-  genre:'hotel',
-  city:'Chagny',
-  description: 'à 2 km du Château'
-  )
-
-Lieu.create(
-  name:'Hotel de la Ferte',
-  phone:'03 85 87 07 47',
-  url:'http://www.hotelferte.com/',
-  genre:'hotel',
-  city:'Chagny',
-  description:'11, boulevard de la Liberté, (6 minutes en voiture, 65 à 70€ la chambre double)'
-  )
-
-
-Lieu.create(
-  name:'Hôtel de la Poste',
-  phone:'03 85 87 64 40',
-  url:'http://www.hoteldelaposte-chagny71.com/',
-  genre:'hotel',
-  city:'Chagny',
-  description: 'à 2 km du Château'
-  )
-
-
-Lieu.create(
-  name:'Château de Bellecroix',
-  phone:'03 85 87 13 86',
-  url:'http://www.chateau-bellecroix.com/',
-  genre:'hotel',
-  city:'Chagny',
-  description: 'à 2 km du Château'
-  )
-
-Lieu.create(
-  name:'La Belle Jardinière Chagny',
-  phone:'09 53 44 07 30',
-  url:'http://www.chambres-hotes.fr/chambres-hotes_chambre-d-hote-de-la-belle-jardiniere_chagny_h420660.htm',
-  genre:'hotel',
-  city:'Chagny',
-  description: 'à 2 km du Château'
-  )
-
-Lieu.create(
-  name:'Hôtel les Hauts de Meursault',
-  phone:'03 80 21 23 47',
-  url:'http://www.hotel-meursault.fr/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château'
-  )
-
-Lieu.create(
-  name:'Hôtel Le Chevreuil',
-  phone:'03 80 21 23 25',
-  url:'http://www.lechevreuil.fr/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Hôtel les Charmes',
-  phone:'03 80 21 63 53',
-  url:'http://www.hotellescharmes.com/fr/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Hôtel La Chouette',
-  phone:'03 80 21 95 60',
-  url:'http://www.la-chouette.fr/fr/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Château de Cîteaux',
-  phone:'03 80 20 62 80',
-  url:'http://www.lacueillette.com',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Hôtel les Arts',
-  phone:'03 80 21 20 28',
-  url:'http://www.hotel-restaurant-les-arts.fr/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Hôtel le Mont Mélian',
-  phone:'03 80 21 64 90',
-  url:'http://www.france-hotel-guide.com/fr/17576-hotel-le-mont-melian-meursault-fr.php',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Hôtel du Centre',
-  phone:'03 80 21 20 75',
-  url:'http://hotel-du-centre-meursault.com/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Chambre d’hôtes Les Ecureuils',
-  phone:'03 80 21 27 82',
-  url:'http://www.lesecureuils-meursault.com/',
-  genre:'hotel',
-  city:'Meursault',
-  description: 'à 7 km du Château')
-
-Lieu.create(
-  name:'Auberge du Camp Romain',
-  phone:'09 70 73 07 91',
-  url:'http://www.auberge-du-camp-romain.com/',
-  genre:'hotel',
-  city:'Chassey-le-Camp',
-  description: 'à 9 km du Château')
-
-Lieu.create(
-  name:'L’Etape à Santenay',
-  phone:'03 80 21 39 43',
-  url:'http://www.etapedesantenay.com/',
-  genre:'hotel',
-  city:'Santenay',
-  description:"10, place du Jet d'Eau (7 minutes en voiture, 85€ la chambre double)"
-  )
-
-Lieu.create(
-  name:'Le Clos du Colombier',
-  phone:'03 80 22 00 27',
-  url:'http://closducolombier.com/fr/index.aspx',
-  genre:'hotel',
-  city:'Pommard',
-  description: 'à 12 km du Château')
-
-Lieu.create(
-  name:'Maison d‘hôtes « Les Nuits de Saint-Jean »',
-  phone:'03 80 22 49 98',
-  url:'http://www.violot-guillemard.fr/maison-hotes/tarifs/',
-  genre:'hotel',
-  city:'Pommard',
-  description: 'à 12 km du Château')
-
-Lieu.create(
-  name:'Hôtel le Panorama',
-  phone:'03 80 26 22 17',
-  url:'http://www.le-panorama.com/fr/index.php',
-  genre:'hotel',
-  city:'Beaune',
-  description: 'à 15 km du Château')
-
-Lieu.create(
-  name:'Hostellerie de Bretonniere',
-  phone:'03 80 22 15 77',
-  url:'http://www.hotelbretonniere.com/fr',
-  genre:'hotel',
-  city:'Beaune',
-  description: 'à 15 km du Château')
-
-Lieu.create(
-  name:'Hôtel Belle Epoque',
-  phone:'09 70 73 06 35',
-  url:'http://www.hotel-belleepoque-beaune.com/fr/accueil-hotel-belle-epoque-beaune.html',
-  genre:'hotel',
-  city:'Beaune',
-  description: 'à 15 km du Château')
-
-Lieu.create(
-  name:'NOVOTEL de Beaune',
-  phone:'03 80 24 59 00',
-  url:'http://www.novotel.com/fr/hotel-1177-novotel-beaune/index.shtml',
-  genre:'hotel',
-  city:'Beaune',
-  description:"16 rue du Moulin Noize (15 minutes en voiture)"
-  )
-
-
-Lieu.create(
-  name:'Mercure de Beaune',
-  phone:'03 80 22 22 00',
-  url:'http://www.mercure.com/fr/hotel-1217-hotel-mercure-beaune-centre/index.shtml',
-  genre:'hotel',
-  city:'Beaune',
-  description:"Avenue Charles de Gaulle (16 minutes en voiture)"
-  )
-
-Lieu.create(
-  name:'Hostellerie de Bretonnière',
-  phone:'03 80 22 15 77',
-  url:'http://www.hotelbretonniere.com/',
-  genre:'hotel',
-  city:'Beaune',
-  description:"43, rue du Faubourg Bretonnière (15 minutes en voiture, 80 à 120€ la chambre double)"
-  )
-
-Lieu.create(
-  name:'Ibis Budget Beaune',
-  phone:'08 92 68 32 71',
-  url:'http://www.ibis.com/fr/hotel-2524-ibis-budget-beaune/index.shtml',
-  genre:'hotel',
-  city:'Beaune',
-  description:"14, rue du Moulin Noize ( 16 minutes en voiture, 45 à 65€ la chambre double)"
-  )
-
+Lieu.create!(name:"Demeure de la Carmine ",city:" Mirepoix ",description:" 30min ",phone:" 06 70 42 11 50 ",url:" http://demeurecarmine.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Domaine de la Trille ",city:" Ste Foi ",description:" 35min ",phone:" 05 61 67 89 82 ",url:" http://www.domainedelatrille.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Domaine Thomson ",city:" Engraviès ",description:" 15min ",phone:" 05 61 60 60 20 ",url:" http://fr.domainethomson.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Domaine la Barre ",city:" Engravies ",description:" 15min ",phone:" 05 61 68 81 20 ",url:" http://www.chambres-hotes-ariege.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Gîte et chambre de Mirepoix ",city:" Mirepoix ",description:" 30min ",phone:" 06 21 01 17 95 ",url:" http://www.gites-mirepoix.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Auberge du Balestie ",city:" Mirepoix ",description:" 30min ",phone:" 05 61 69 58 64 ",url:" http://www.auberge-du-balestie.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Abbaye de Camon ",city:" Camon ",description:" 35min ",phone:" 05 61 60 31 23 ",url:" http://www.chateaudecamon.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Mi René ",city:" Lagarde ",description:" 25min ",phone:" 05 61 68 98 46 ",url:" http://www.ariege.com/mi-rene/",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Domaine du Hameau ",city:" St Jean d’Aiguë vive ",description:" 25min ",phone:" 05 34 14 63 65 ",url:" http://www.hameaubaylesse.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"L’impasse du Temps ",city:" Léran ",description:" 15min ",phone:" 05 61 01 50 02 ",url:" http://www.chezfurness.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"La maison Rose ",city:" Léran ",description:" 15min ",phone:" 04 68 31 05 73 ",url:" http://www.lac-de-montbel.com/fr",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Santa Colomba ",city:" Ste Colombe sur l’Hers ",description:" 20min ",phone:" 06 80 10 56 24 ",url:" http://www.santa-colomba.fr",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Aux quatre saisons ",city:" Tabre ",description:" 15min ",phone:" 05.61.01.81.14 ",url:" http://www.aux4saisons09.com",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Le Labyrinthe ",city:" Montségur (château) ",description:" 35min ",phone:" ",url:"",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Le café du Théâtre ",city:" Lavelanet ",description:" 20min ",phone:" 06 87 46 53 60",url:"",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Les Sabines ",city:" Montgaillard ",description:" 30min ",phone:"",url:"",genre:"Chambre d\’hôtes")
+Lieu.create!(name:"Domaine la Barre (2 à 7 pers) ",city:" Engravies ",description:" 15min ",phone:" 05 61 68 81 20 ",url:" http://www.chambres-hotes-ariege.com",genre:"Gîte")
+Lieu.create!(name:"La maison du Roy (4 à 8 pers) ",city:" Lieurac ",description:" 5min",phone:"",url:"",genre:"Gîte")
+Lieu.create!(name:"A Roquefort les cascades (6pers) ",city:" (Piscine) ",description:" 10min",phone:"",url:"",genre:"Gîte")
+Lieu.create!(name:"La maison de roquefort ",city:" Carla de Roquefort ",description:" 5min ",phone:" 05 61 01 21 29",url:"",genre:"Gîte")
+Lieu.create!(name:"Ventenac : Marinette et Robert Comelongue (piscine) ",city:"Ventenac",description:" 5min ",phone:" 05 61 60 72 43 ",url:"",genre:"Camping")
+Lieu.create!(name:"Hôtel les Minotiers ",city:" Mirepoix ",description:" 30min ",phone:" 05 61 69 37 36 ",url:" http://www.hotelmirepoix.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel le Commerce ",city:" Mirepoix ",description:" 30min ",phone:" 05 61 68 10 29 ",url:" http://www.le-commerce.fr",genre:"Hôtel")
+Lieu.create!(name:"Hôtel les remparts ",city:" Mirepoix ",description:" 30min ",phone:" 05 61 68 12 15 ",url:" http://www.hotelremparts.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel la maison des consuls ",city:" Mirepoix ",description:" 30min ",phone:" 05 61 68 81 81 ",url:" http://www.maisondesconsuls.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel du rélais royal ",city:" Mirepoix ",description:" 30min ",phone:" 05 61 60 19 19 ",url:"http://www.relaisroyal.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Costes ",city:" Montségur ",description:" 35min ",phone:" 05 61 02 66 21",url:"",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Couquet ",city:" Montségur ",description:" 35min ",phone:" 05 61 01 10 28 ",url:"",genre:"Hôtel")
+Lieu.create!(name:"Hôtel de France ",city:" Chalabre ",description:" 35min",phone:"",url:"",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Castel d’Olmes ",city:" Laroque d’Olmes ",description:" 15min ",phone:" 05 61 01 04 58 ",url:" http://www.casteldolmes.fr",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Castrum ",city:" Villeneuve d’Olmes ",description:" 25min ",phone:" 05 61 04 44 00 ",url:" http://www.lecastrum.fr",genre:"Hôtel")
+Lieu.create!(name:"Auberge la Barguillère ",city:" Saint Pierre de rivière ",description:" 35min ",phone:" 05 61 65 14 02 ",url:" http://www.barguillere.fr",genre:"Hôtel")
+Lieu.create!(name:"Auberge Léo ",city:" Foix ",description:" 30min ",phone:" 05 61 65 09 04 ",url:" http://www.leodefoix.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Balladins ",city:" Foix ",description:" 30min ",phone:" 05 61 05 36 10 ",url:" https://www.balladins.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Eychenne ",city:" Foix ",description:" 30min ",phone:" 05 61 04 04 50 ",url:" http://www.ariege.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel du Lac ",city:" Foix ",description:" 30min ",phone:" 05 61 65 17 17 ",url:" http://www.hoteldulac-foix.fr",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Lons ",city:" Foix ",description:" 30min ",phone:" 05 34 09 28 00 ",url:" http://www.hotel-lons-foix.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel de France ",city:" Pamiers ",description:" 30min ",phone:" 05 61 60 20 88 ",url:" http://hotel-de-France-pamiers.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel de la paix ",city:" Pamiers ",description:" 30min ",phone:" 05 61 67 12 71 ",url:" http://hoteldelapaix-pamiers.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel le Roi Gourmand ",city:" Pamiers ",description:" 30min ",phone:" 05 61 60 12 12 ",url:" http://www.hotel-roigourmand09.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Rocade ",city:" Pamiers ",description:" 30min ",phone:" 05 34 02 12 13 ",url:" http://www.hotel-ma-rocade-pamiers.com",genre:"Hôtel")
+Lieu.create!(name:"Hôtel Première Classe ",city:" Pamiers ",description:" 30min ",phone:" 0 892 70 71 86 ",url:" http://www.premiereclasse.com/fr/hotels/premiere-classe-pamiers",genre:"Hôtel")
